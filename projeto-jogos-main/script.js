f5b2b7cfc490e67626511605832e7879c3f28d54
Dudu <127527178+carlosbasseto@users.noreply.github.com>
2024-01-10 15:19:15 -0300

    Add files via upload

---
+++ projeto-jogos-main/script.js
@@ -0,0 +1,88 @@
+import getWord from "./words.js";
+
+const contentBtns = document.querySelector(".btns");
+const contentGuessWord = document.querySelector(".guess-word");
+const img = document.querySelector("img");
+const contentClue = document.querySelector(".clue");
+const btnNew = document.querySelector(".new");
+btnNew.onclick = () => init();
+let forcaImg;
+
+init();
+
+function init() {
+  forcaImg = 1;
+  img.src = `img1.png`;
+
+  generateGuessSection();
+  generateButtons();
+}
+
+function generateGuessSection() {
+  contentGuessWord.textContent = "";
+
+  const { word, clue } = getWord();
+  const wordWithoutAccent = word
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+
+  Array.from(wordWithoutAccent).forEach((letter) => {
+    const span = document.createElement("span");
+
+    span.textContent = "_";
+    span.setAttribute("word", letter.toUpperCase());
+    contentGuessWord.appendChild(span);
+  });
+
+  contentClue.textContent = `Dica: ${clue}`;
+}
+
+function wrongAnswer() {
+  forcaImg++;
+  img.src = `img${forcaImg}.png`;
+
+  if (forcaImg === 7) {
+    setTimeout(() => {
+      alert("Perdeu :/");
+      init();
+    }, 100);
+  }
+}
+
+function verifyLetter(letter) {
+  const arr = document.querySelectorAll(`[word="${letter}"]`);
+
+  if (!arr.length) wrongAnswer();
+
+  arr.forEach((e) => {
+    e.textContent = letter;
+  });
+
+  const spans = document.querySelectorAll(`.guess-word span`);
+  const won = !Array.from(spans).find((span) => span.textContent === "_");
+
+  if (won) {
+    setTimeout(() => {
+      alert("Ganhou!!!");
+      init();
+    }, 100);
+  }
+}
+
+function generateButtons() {
+  contentBtns.textContent = "";
+
+  for (let i = 97; i < 123; i++) {
+    const btn = document.createElement("button");
+    const letter = String.fromCharCode(i).toUpperCase();
+    btn.textContent = letter;
+
+    btn.onclick = () => {
+      btn.disabled = true;
+      btn.style.backgroundColor = "gray";
+      verifyLetter(letter);
+    };
+
+    contentBtns.appendChild(btn);
+  }
+}
